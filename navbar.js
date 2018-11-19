7f30dd9de3be8733042743c34d81ab87426298e6
Viterlen <tomasz670zielinski@gmail.com>
2018-11-19 17:51:40 +0100

    add new nav

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<nav><ul class="nav"><li><a href="index.html">Strona Główna</a></li>' +
+    '<li><a href="kontakt.html">Kontakt</a></li></ul> </nav>';
+
+let divNav = document.querySelector(".navbar");
+divNav.innerHTML = nav;
+
+
