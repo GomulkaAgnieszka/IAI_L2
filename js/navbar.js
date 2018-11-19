c18f5c892fa267b2664166355547eb5b1ed43ee8
Viterlen <tomasz670zielinski@gmail.com>
2018-11-19 18:06:35 +0100

    segregate

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,5 @@
 let nav = '<nav><ul class="nav"><li><a href="index.html">Strona Główna</a></li>' +
+    '<li><a href="map.html">Mapa</a></li>'+
     '<li><a href="kontakt.html">Kontakt</a></li></ul> </nav>';
 
 let divNav = document.querySelector(".navbar");
