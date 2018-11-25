a52a0b3232146192a6d29b8abeecffcc31a8d423
MateuszKorzeniowski <MateuszKorzeniowski@users.noreply.github.com>
2018-11-25 21:45:47 +0100

    add ref

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,7 @@
 let nav = '<nav><ul class="nav"><li><a href="index.html">Strona Główna</a></li>' +
     '<li><a href="map.html">Mapa</a></li>'+
-    '<li><a href="kontakt.html">Kontakt</a></li></ul> </nav>';
+    '<li><a href="index_quiz.html">Quiz</a></li>'+
+    '<li><a href="kontakt.html">Kontakt</a></li></ul></nav>';
 
 let divNav = document.querySelector(".navbar");
 divNav.innerHTML = nav;
